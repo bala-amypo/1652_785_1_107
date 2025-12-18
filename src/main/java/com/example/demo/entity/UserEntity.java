7b67c954bd626b5c4a89cedfd123a8ba590f942d
bala-amypo <bala@amypo.in>
2025-12-18 17:23:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,5 +1,9 @@
 package com.example.demo.entity;
 
 public class UserEntity {
-    
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
 }
