6837011367ebcf0b1e78f79e91f282dbd6415eb3
bala-amypo <bala@amypo.in>
2025-12-18 16:59:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,12 @@
 package com.example.demo.controller;
-
+@RestController
 public class AuthController {
     
+    @Autowired UserService service;
+
+    @PostMapping("/post")
+    public UserEntity senddata(@RequestBody UserEntity e){
+        return service.saveuser(e);
+    }
+    @Get
 }
