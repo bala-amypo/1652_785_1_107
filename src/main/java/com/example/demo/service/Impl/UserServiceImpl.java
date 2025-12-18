f6bb216581b8579ba76650ef61300e5b5591b727
bala-amypo <bala@amypo.in>
2025-12-18 16:27:11 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -1,1 +1,8 @@
-package com.
+package com.example.demo.service.Impl;
+
+public class UserServiceImpl implements UserService {
+    @Autowired UserRepository users;
+
+    @PostMapping("/post")
+    public UserEntity postdata()
+}
