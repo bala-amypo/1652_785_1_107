26e47b72ce1a33bf1906faf138e883da92cf1741
bala-amypo <bala@amypo.in>
2025-12-19 08:44:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/ParcelEntity.java
+++ src/main/java/com/example/demo/entity/ParcelEntity.java
@@ -1,5 +1,23 @@
 package com.example.demo.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.AllArgsConstructor;'
+import 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ParcelEntity {
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
 }
