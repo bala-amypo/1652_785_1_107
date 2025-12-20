2d5d8bb3eb4fcbcdb8395fdeefc4724082dd56fe
bala-amypo <bala@amypo.in>
2025-12-20 15:14:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.User;
-import com.example.demo.security.JwtUtil;
+import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -11,35 +10,29 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
-    private final JwtUtil jwtUtil;
     private final PasswordEncoder passwordEncoder;
 
     public AuthController(UserService userService,
-                          JwtUtil jwtUtil,
                           PasswordEncoder passwordEncoder) {
         this.userService = userService;
-        this.jwtUtil = jwtUtil;
         this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
-    public User register(@RequestBody User user) {
+    public UserEntity register(@RequestBody UserEntity user) {
         return userService.register(user);
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody User request) {
+    public UserEntity login(@RequestBody UserEntity request) {
 
-        User user = userService.findByEmail(request.getEmail());
+        UserEntity user = userService.findByEmail(request.getEmail());
 
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new RuntimeException("Invalid credentials");
         }
 
-        return jwtUtil.generateToken(
-                user.getId(),
-                user.getEmail(),
-                user.getRole()
-        );
+        
+        return user;
     }
 }
--- src/main/java/com/example/demo/service/DamageClaimService.java
+++ src/main/java/com/example/demo/service/DamageClaimService.java
@@ -1,9 +1,9 @@
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import com.example.demo.model.DamageClaimEntity;
+// import com.example.demo.model.DamageClaimEntity;
 
-public interface DamageClaimService {
-    DamageClaimEntity fileClaim(Long parcelId, DamageClaimEntity claim);
-    DamageClaimEntity evaluateClaim(Long claimId);
-    DamageClaimEntity getClaim(Long claimId);
-}
+// public interface DamageClaimService {
+//     DamageClaimEntity fileClaim(Long parcelId, DamageClaimEntity claim);
+//     DamageClaimEntity evaluateClaim(Long claimId);
+//     DamageClaimEntity getClaim(Long claimId);
+// }
