317edc001f0c422b4d3e7b30629152f0601902fd
bala-amypo <bala@amypo.in>
2025-12-20 14:53:57 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.demo.service.impl;
 
 import com.example.demo.exception.BadRequestException;
 import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.entity.User;
+import com.example.demo.entity.UserEntity;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,7 +18,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User register(User user) {
+    public UserEntity register(UserEntity user) {
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new BadRequestException("User already exists");
         }
@@ -32,7 +32,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User findByEmail(String email) {
+    public UserEntity findByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
