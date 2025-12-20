a8bdcbd81d6c46180e995c4808dea4e38e27f49f
bala-amypo <bala@amypo.in>
2025-12-20 15:41:57 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.Impl;
 
-import com.example.demo.entity.UserEntity;
+import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
@@ -16,7 +16,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserEntity register(UserEntity user) {
+    public User register(User user) {
 
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new RuntimeException("User already exists");
