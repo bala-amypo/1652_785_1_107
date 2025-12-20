d50857e8afd1f5b23a1552190fd982e18405c191
bala-amypo <bala@amypo.in>
2025-12-20 17:57:36 +0000

    User commit

--- src/main/java/com/example/demo/model/Evidence.java
+++ src/main/java/com/example/demo/model/Evidence.java
@@ -1,4 +1,22 @@
-@ManyToOne(optional = false)
+package com.example.demo.entity;
+
+
+
+
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Evidence {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
     @JoinColumn(name = "claim_id")
     private DamageClaim claim;
 
@@ -7,3 +25,5 @@
     private String fileUrl;
 
     private LocalDateTime uploadedAt;
+
+}
