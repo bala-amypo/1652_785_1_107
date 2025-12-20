c98dad0c983aaad3aae4ca6669d028130246c8cf
bala-amypo <bala@amypo.in>
2025-12-20 17:03:02 +0000

    User commit

--- src/main/java/com/example/demo/model/ClaimRule.java
+++ src/main/java/com/example/demo/model/ClaimRule.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstrutor
 @NoArgsConstructor
-public class ClaimRuleEntity {
+public class ClaimRule {
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
