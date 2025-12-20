a238ac63080a202a31bc49ce5041cf709e63c214
bala-amypo <bala@amypo.in>
2025-12-20 07:19:36 +0000

    User commit

--- src/main/java/com/example/demo/service/EvidenceService.java
+++ src/main/java/com/example/demo/service/EvidenceService.java
@@ -1,20 +1,9 @@
 package com.example.demo.service;
 
-
-
-import java.util.List;
 import com.example.demo.entity.EvidenceEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
-
+import java.util.List;
 
 public interface EvidenceService {
-       EvidenceEntity saveevidence(@RequestBody EvidenceEntity evi);
-       List<EvidencelEntity>getevidencedata();
-       String Deleteevidence( Long id);
-       ParcelEntity evidencedata( Long id);
-       ParcelEntity postevidence(Long id,EvidenceEntity Evidence);
-
-      
+    EvidenceEntity uploadEvidence(Long claimId, EvidenceEntity evidence);
+    List<EvidenceEntity> getEvidenceForClaim(Long claimId);
 }
