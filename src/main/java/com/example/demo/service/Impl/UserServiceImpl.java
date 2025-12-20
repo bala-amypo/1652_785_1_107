8da524c015952b459fabae0ba4ac3ad03cd20ce5
bala-amypo <bala@amypo.in>
2025-12-20 15:35:57 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -5,18 +5,18 @@ import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
 
-@Service   // ✅ THIS IS THE KEY FIX
+@Service   
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    // REQUIRED constructor
+   
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
-    public User register(User user) {
+    public UserEntity register(UserEntity user) {
 
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new RuntimeException("User already exists");
@@ -30,7 +30,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User findByEmail(String email) {
+    public UserEntity findByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
