dca524a2bed5594e02a1381a07f8ba3d2615bc7b
bala-amypo <bala@amypo.in>
2025-12-20 05:58:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/DamageClaimEntity.java
+++ src/main/java/com/example/demo/entity/DamageClaimEntity.java
@@ -1,5 +1,27 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DamageClaimEntity {
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String parcel;
+    private String claimDescription;
+    private String receiverName;
+    private Double weightKg;
+    private LocalDateTime deliveredAt;
+
 }
--- src/main/java/com/example/demo/entity/ParcelEntity.java
+++ src/main/java/com/example/demo/entity/ParcelEntity.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 @Entity
 @Data
 @AllArgsConstructor
