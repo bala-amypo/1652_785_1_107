c72cdf25c14508ad14aaa1454d17f383d249f9fc
bala-amypo <bala@amypo.in>
2025-12-20 18:11:58 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EvidenceServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EvidenceServiceImpl.java
@@ -6,7 +6,7 @@ import com.example.demo.model.Evidence;
 import com.example.demo.repository.DamageClaimRepository;
 import com.example.demo.repository.EvidenceRepository;
 import com.example.demo.service.EvidenceService;
-
+import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class EvidenceServiceImpl implements EvidenceService {
