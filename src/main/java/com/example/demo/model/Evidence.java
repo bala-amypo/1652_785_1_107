0bca471ed33a5442a2211556dd6e80b7a0f8af52
bala-amypo <bala@amypo.in>
2025-12-21 15:35:37 +0000

    User commit

--- src/main/java/com/example/demo/model/Evidence.java
+++ src/main/java/com/example/demo/model/Evidence.java
@@ -22,7 +22,7 @@ public class Evidence {
     @JoinColumn(name = "claim_id")
     private DamageClaim claim;
 
-    private String evidenceType; // IMAGE | DOCUMENT | TEXT
+    private String evidenceType; 
 
     private String fileUrl;
 
