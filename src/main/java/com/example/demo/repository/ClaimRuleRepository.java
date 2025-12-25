e2769b8938ec0d7ccac65078e060d2c02dac25f5
bala-amypo <bala@amypo.in>
2025-12-25 14:05:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClaimRuleRepository.java
+++ src/main/java/com/example/demo/repository/ClaimRuleRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.ClaimRule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-ublic interface ClaimRuleRepository extends JpaRepository<ClaimRule, Long> {
-
+@Repository
+public interface ClaimRuleRepository extends JpaRepository<ClaimRule, Long> {
+    // findAll() is inherited from JpaRepository
 }
